ca8b0ad5c5c3bd1f1866d3f0e683b9dc9db97715
gabrielborgesmagalhaes <gabrielbm06@gmail.com>
2017-03-15 19:46:43 -0300

    atualizacao do comando perf no run.sh

--- programa/run.sh
+++ programa/run.sh
@@ -4,5 +4,11 @@
 #gera graficos do gprof
 # gprof solver | python gprof2dot.py -n0 -e0 | dot -Tpng -o output.png
 
-#cache misses
-# perf stat -B -e cache-references,cache-misses,cycles,instructions,branches,faults,migrations ./solver < tests/test0
+# 3 iteracoes do perf para calcularmos a media dos valoresp posteriormente
+echo 'Primeira iteração do perf'
+perf stat -d -e instructions,cycles,L1-dcache-loads,L1-dcache-load-misses,dTLB-load-misses,cache-references,branch-misses,branch-instructions ./solver > perfOutput1.out 2>&1
+echo 'Segunda iteração do perf'
+perf stat -d -e instructions,cycles,L1-dcache-loads,L1-dcache-load-misses,dTLB-load-misses,cache-references,branch-misses,branch-instructions ./solver > perfOutput2.out 2>&1
+echo 'Terceira iteração do perf'
+perf stat -d -e instructions,cycles,L1-dcache-loads,L1-dcache-load-misses,dTLB-load-misses,cache-references,branch-misses,branch-instructions ./solver > perfOutput3.out 2>&1
+
